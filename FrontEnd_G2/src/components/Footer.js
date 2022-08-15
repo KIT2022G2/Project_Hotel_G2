d618d05e530b9af5bac27f1aaee1f7ebb752cc73
yourlowkey <m.phuc2552000@gmail.com>
2022-08-15 12:30:55 +0700

    thua ke project cu

---
+++ FrontEnd_G2/src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
+import { IoLogoYoutube } from 'react-icons/io';
+import { AiFillInstagram } from 'react-icons/ai';
+
+const Footer = () => {
+    return (
+        <footer id="sticky-footer" className="bg-dark">
+            <div className="container py-1">
+                <div className="row">
+                     <div className="col-md-6 col-12">
+                        <div>
+                            <span>Số điện thoại: <span> </span><a href="tel:0869.165.610" class="phone-number">0869.165.610</a></span>
+                        </div>
+                        <div>
+                            <span>Email: <span> </span><a href="mailto:bosua12@gmail.com" class="email">bosua12@gmail.com</a></span>
+                        </div>
+                    </div> 
+                    <div className="col-md-6 col-12">
+                        <div className="d-flex float-right">
+
+                            <FaFacebookSquare className="connect text-primary" />
+
+                            <FaLinkedin className="connect text-info" />
+
+                            <AiFillInstagram className="connect text-warning" />
+
+                            <IoLogoYoutube className="connect text-danger" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
---
+++ FrontEnd_G2/src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import {FaRegMeh} from 'react-icons/fa';
+
+export default function Error() {
+    return (
+        <>
+        <Hero hero="roomsError" />
+         <Banner title="ERROR 404 NOT FOUND" subtitle= "Opp!!! Tìm kiếm của bạn không khả dụng">
+                <FaRegMeh className="lost"></FaRegMeh><span> </span>
+                <Link to="/" className="btn btn-warning">
+                <i class="fa fa-exclamation-circle" aria-hidden="true"></i> Quay về trang chủ
+                </Link>
+        </Banner>
+        </>
+    )
+}
