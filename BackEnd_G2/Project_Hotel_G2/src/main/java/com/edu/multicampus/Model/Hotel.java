30c00dc8a874e4cb983c6e543fb07bc7c54fb6fb
yourlowkey <m.phuc2552000@gmail.com>
2022-08-16 16:10:40 +0700

    HotelService implement complete

--- BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Model/Hotel.java
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Model/Hotel.java
@@ -19,6 +19,69 @@ public class Hotel {
 	@Column(name="hotellocation",nullable=false)
 	private String hotelLocation;
 	
+	public Hotel() {
+		super();
+	}
+
+	public Hotel(Long hotelId, String hotelName, String hotelLocation, List<HotelRating> hotelrating, List<Room> room,
+			List<Staff> staff) {
+		super();
+		this.hotelId = hotelId;
+		this.hotelName = hotelName;
+		this.hotelLocation = hotelLocation;
+		this.hotelrating = hotelrating;
+		this.room = room;
+		this.staff = staff;
+	}
+
+	public Long getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(Long hotelId) {
+		this.hotelId = hotelId;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public String getHotelLocation() {
+		return hotelLocation;
+	}
+
+	public void setHotelLocation(String hotelLocation) {
+		this.hotelLocation = hotelLocation;
+	}
+
+	public List<HotelRating> getHotelrating() {
+		return hotelrating;
+	}
+
+	public void setHotelrating(List<HotelRating> hotelrating) {
+		this.hotelrating = hotelrating;
+	}
+
+	public List<Room> getRoom() {
+		return room;
+	}
+
+	public void setRoom(List<Room> room) {
+		this.room = room;
+	}
+
+	public List<Staff> getStaff() {
+		return staff;
+	}
+
+	public void setStaff(List<Staff> staff) {
+		this.staff = staff;
+	}
+
 	@OneToMany(mappedBy="hotel")
 	private List<HotelRating> hotelrating;
 	
