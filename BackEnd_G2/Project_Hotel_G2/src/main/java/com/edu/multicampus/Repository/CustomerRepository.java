eef708f9b71b879a04d53c3b03a50d19df8e7fe1
huyd84 <huyduong0849@gmail.com>
2022-08-16 14:21:12 +0700

    fix repo

--- BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/CustomerRepository.java
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/CustomerRepository.java
@@ -1,12 +1,11 @@
 package com.edu.multicampus.Repository;
 
 import com.edu.multicampus.Model.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerRepository, Long> {
-    Page<CustomerRepository> findAll(Pageable pageable);
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  
 }
--- BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/StaffRepository.java
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/StaffRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.edu.multicampus.Model.Staff;
 
+
+
 public interface StaffRepository extends JpaRepository<Staff, Long> {
 
 }
