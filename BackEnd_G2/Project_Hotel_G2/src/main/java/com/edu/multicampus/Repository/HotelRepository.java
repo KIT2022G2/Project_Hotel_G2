9eaee4c9a25e095e9e859344d4354aa4c365a2a2
yourlowkey <m.phuc2552000@gmail.com>
2022-08-16 14:48:12 +0700

    HotelService interface

--- BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/HotelRepository.java
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/HotelRepository.java
@@ -3,6 +3,8 @@ package com.edu.multicampus.Repository;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,5 @@ import com.edu.multicampus.Model.Hotel;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
-	
+	 Page<Hotel> findAll(Pageable pageable);
 }
--- BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Services/HotelService.java
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Services/HotelService.java
@@ -1,8 +1,16 @@
 package com.edu.multicampus.Services;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
 import com.edu.multicampus.Model.Hotel;
 
 public interface HotelService {
-	void save(Hotel hotel);
-	
+	List<Hotel> getAllHotels();
+	int addHotel(Hotel hotel);
+	int deleteHotelById (long id);
+	Hotel getHotelById (long id);
+	Page<Hotel> getAllCard(int pageNo, int pageSize);
+	int updateHotelById (long id, Hotel hotel);
 }
