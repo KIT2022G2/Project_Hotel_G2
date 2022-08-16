7c6fa12e9b1773bce66c8461d4761835905a38e1
Talatam1998 <talatam.1998@gmail.com>
2022-08-16 12:32:37 +0700

    3

---
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/BillRepository.java
@@ -0,0 +1,9 @@
+package com.edu.multicampus.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edu.multicampus.Model.Bill;
+
+public interface BillRepository  extends JpaRepository<Bill, Long> {
+
+}
---
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/HotelRatingRepository.java
@@ -0,0 +1,9 @@
+package com.edu.multicampus.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edu.multicampus.Model.HotelRating;
+
+public interface  HotelRatingRepository extends JpaRepository<HotelRating, Long> {
+
+}
