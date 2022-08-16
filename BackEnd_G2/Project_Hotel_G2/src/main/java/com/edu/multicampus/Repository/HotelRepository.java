790cdd3e3866635daac7189003ce448442ad415b
yourlowkey <m.phuc2552000@gmail.com>
2022-08-16 14:19:00 +0700

    package Service init

---
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Repository/HotelRepository.java
@@ -0,0 +1,14 @@
+package com.edu.multicampus.Repository;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edu.multicampus.Model.Hotel;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+	
+}
---
+++ BackEnd_G2/Project_Hotel_G2/src/main/java/com/edu/multicampus/Services/HotelService.java
@@ -0,0 +1,8 @@
+package com.edu.multicampus.Services;
+
+import com.edu.multicampus.Model.Hotel;
+
+public interface HotelService {
+	void save(Hotel hotel);
+	
+}
